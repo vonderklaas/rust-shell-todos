36d6136e868f9c23afbd7ec3be546965af89d66b
Nick Garbalau <garbalaunick@gmail.com>
2023-05-05 10:17:24 +0300

    Added todo printing and basic coloring

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,62 @@
 extern crate ncurses;
 use ncurses::*;
 
+/* Constants. */
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
 fn main () {
 
-  /* Start ncurses. */
-  initscr();
+    /* Init ncurses. */
+    initscr();
 
-  /* Print to the back buffer. */
-  addstr("Hello, world!");
+    /* Init colors. */
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
 
-  /* Update the screen. */
-  refresh();
+    /* Init application state. */
+    let mut quit= false;
+    let mut todos =vec![
+        "Buy a bread",
+        "Write the todo app",
+        "Make a cup of tea"
+    ];
+    let todo_curr: usize =  0;
 
-  /* Wait for a key press. */
-  getch();
+    /* Event loop. */
+    while !quit {
+        for (index, todo) in todos.iter().enumerate() {
 
-  /* Terminate ncurses. */
-  endwin();
+            /* Colors. */
+            let pair = {
+                if todo_curr == index {
+                    /* Render in a different style. */
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                }
+            };
+
+            attr_on(COLOR_PAIR(pair));
+            mv(index as i32, 0);
+            addstr(*todo);
+            attr_off(COLOR_PAIR(pair));
+        }
+
+        /* Update the screen. */
+        refresh();
+
+        /* Wait for a key press. */
+        let key = getch();
+
+        /* Handle input from the user. */
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => {}
+        }
+    }
+
+    /* Terminate ncurses. */
+    endwin();
 }
